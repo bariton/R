62b3491ad92b84b413a90fe5b7101f1167c7f86d
bariton <y.dan.1993@gmail.com>
2013-12-04 20:22:20 +0300

    initial commit

---
+++ программка 1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Output(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+                Console.Write(a[i] + " ");
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            
+            int[] mas = new int[10];
+            for (int i = 0; i != 10; i++)
+            {
+                Console.WriteLine("Введите элемент массива {0}", i);
+                mas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Ваш массив");
+            Output(mas);
+
+            int N, K=0;
+            Console.WriteLine("введите N");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (N == mas[i])
+                {
+                    
+                    Console.WriteLine("i=" + i);
+                   
+                    K += 1;
+                    
+                    
+                }
+                
+            }
+            if(K==0)
+            {
+                Console.WriteLine("-1");
+            }
+
+
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ программка2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[,] mas = new int[12, 12] { { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 }, { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 }, { 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9 }, { 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9 }, { 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9 }, { 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9 }, { 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9 }, { 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9 }, { 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9 }, { 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9 }, { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 }, { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 }, };
+
+
+
+            int X, X1, Y, Y1;
+            Console.WriteLine("Введите Х от 1 до 8");
+            X = Convert.ToInt32(Console.ReadLine()) + 1;
+            Console.WriteLine("Введите Y от 1 до 8");
+            Y = Convert.ToInt32(Console.ReadLine()) + 1;
+            Console.WriteLine("Введите Х1 от 1 до 8");
+            X1 = Convert.ToInt32(Console.ReadLine()) + 1;
+            Console.WriteLine("Введите Y1 от 1 до 8");
+            Y1 = Convert.ToInt32(Console.ReadLine()) + 1;
+
+
+
+            mas[X, Y] = 0;
+            int Ni = 0;
+            for (int M = 0; M < 6; M++)
+            {
+
+                for (int i = 0; i < 12; i++)
+                {
+                    for (int j = 0; j < 12; j++)
+                    {
+                        if (mas[i, j] == M)
+                        {
+                            if (mas[i + 2, j + 1] == 8)
+                            {
+                                mas[i + 2, j + 1] = Ni + 1;
+                            }
+                            if (mas[i + 2, j - 1] == 8)
+                            {
+                                mas[i + 2, j - 1] = Ni + 1;
+                            }
+                            if (mas[i - 2, j + 1] == 8)
+                            {
+                                mas[i - 2, j + 1] = Ni + 1;
+                            }
+                            if (mas[i - 2, j - 1] == 8)
+                            {
+                                mas[i - 2, j - 1] = Ni + 1;
+                            }
+                            if (mas[i + 1, j + 2] == 8)
+                            {
+                                mas[i + 1, j + 2] = Ni + 1;
+                            }
+                            if (mas[i + 1, j - 2] == 8)
+                            {
+                                mas[i + 1, j - 2] = Ni + 1;
+                            }
+                            if (mas[i - 1, j - 2] == 8)
+                            {
+                                mas[i - 1, j - 2] = Ni + 1;
+                            }
+                            if (mas[i - 1, j + 2] == 8)
+                            {
+                                mas[i - 1, j + 2] = Ni + 1;
+                            }
+                        }
+                    }
+                }
+                Ni += 1;
+            }
+            for (int i = 0; i < 12; i++)
+            {
+                for (int j = 0; j < 12; j++)
+                {
+                    Console.Write(mas[i, j] + " ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.WriteLine("i=" + mas[X1, Y1]);
+            Console.ReadKey();
+
+        }
+    }
+}
